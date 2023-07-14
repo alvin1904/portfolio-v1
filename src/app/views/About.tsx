694a449a71275c3f45595714944a544b18e6e5e9
alvin <alvin19official@gmail.com>
2023-07-14 19:57:45 +0530

    feat: about view styling
    - about components styling
    - about responsiveness

--- src/app/views/About.tsx
+++ src/app/views/About.tsx
@@ -5,24 +5,33 @@ import PersonalInfo from "@/components/about/PersonalInfo";
 import Skills from "@/components/about/Skills";
 import Experience from "@/components/about/Experience";
 import Education from "@/components/about/Education";
+import { resumeLink } from "@/data/links";
 
 export default function About() {
+  const openResume = () => window.open(resumeLink, "_blank");
   return (
     <div className="view bg">
-      <section className="section">
-        <button className={styles.cvbtn}>
-          <div>
-            <p>Download</p>
+      <section className={styles.section}>
+        <button className={styles.cvbtn + " bg-inverse"} onClick={openResume}>
+          <div className={styles.text}>
             <p>Resume</p>
           </div>
           <div>
-            <IonIcon icon={cloudDownloadOutline} aria-label="resume" />
+            <IonIcon
+              className={styles.icon}
+              icon={cloudDownloadOutline}
+              aria-label="resume"
+            />
           </div>
         </button>
-        <PersonalInfo />
-        <Skills />
-        <Experience />
-        <Education />
+        <div className={styles.grouper}>
+          <PersonalInfo />
+          <Skills />
+        </div>
+        <div className={styles.grouper}>
+          <Education />
+          <Experience />
+        </div>
       </section>
     </div>
   );
--- src/data/content.tsx
+++ src/data/content.tsx
@@ -1,6 +1,6 @@
 export const personalInfo: string[] = [
-  "Hello! My name is Alvin and I have a passion for creating innovative frontends for the internet. Since starting my journey in web design and development in September 2022, I have fallen in love with the process. I take pride in writing structured and creative CSS for my frontend projects",
-  "I have built a number of interesting and creative projects, including a music streaming website and a points management website, among others. You can check them out in the Projects section. I am always striving to push the boundaries of what is possible with my work.",
+  "Hello! My name is Alvin and I have a passion for creating innovative frontends for the internet. Since starting my journey in web design and development in September 2022, I have fallen in love with the process.",
+  "I have built a number of interesting and creative projects, including a music streaming website and a points management website, among others. I am always striving to push the boundaries of what is possible with my work.",
 ];
 export const skills: string[] = [
   "Javascript (ES6+)",
@@ -14,5 +14,6 @@ export const education: string[] = [
   "Rajiv Gandhi Institute of Technology, Kottayam",
 ];
 export const experience: string[] = [
-  "My journey began as a graphic designer and I interned at Revertech IT Solutions Pvt. Ltd. I am a member of the web teams for TEDx and IEDC in my college. I have also contributed to web design and frontend development for various IEDC web projects.",
+  "My journey began as a graphic designer and I interned at Revertech IT Solutions Pvt. Ltd.",
+  "I am a member of the web teams for TEDx and IEDC in my college. I have also contributed to web design and frontend development for various IEDC web projects.",
 ];
