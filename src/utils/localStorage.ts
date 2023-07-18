d6177009a48f6b909d5bd256744b76c473ba631b
alvin <alvin19official@gmail.com>
2023-07-18 08:45:12 +0530

    fix: ls error issue

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,15 +1,22 @@
 export function getFromLS(key: string): any | null {
+  try {
     if (typeof window !== "undefined") {
       const value = localStorage.getItem(key);
       if (value) return JSON.parse(value);
     }
-    return null;
+  } catch (e) {
+    console.error(e);
   }
-  
-  export function sendToLS(key: string, value: any): void {
+  return null;
+}
+
+export function sendToLS(key: string, value: any): void {
+  try {
     if (typeof window !== "undefined") {
       const temp = JSON.stringify(value);
       localStorage.setItem(key, temp);
     }
+  } catch (e) {
+    console.error(e);
   }
-  
+}
