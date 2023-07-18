50493fd32244a39b62696cb9aa088ee101447540
alvin <alvin19official@gmail.com>
2023-07-18 08:34:39 +0530

    feat: fetching portfolio link from env

--- src/data/links.tsx
+++ src/data/links.tsx
@@ -1,5 +1,4 @@
 export const githubLink = "https://github.com/alvin1904";
 export const linkedinLink = "https://www.linkedin.com/in/alvin1904/";
 export const instaLink = "https://www.instagram.com/alvin190402/";
-export const resumeLink =
-  "https://drive.google.com/file/d/1xKINpVybKCKkzrBL0TqgQWhCzkcp12nm/view?usp=sharing";
+export const resumeLink = process.env.NEXT_PUBLIC_PORTFOLIO_LINK;
